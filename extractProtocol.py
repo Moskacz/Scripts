495632c3f9b736d0bab11fdb56ce2e4f3d98b233
Michał Moskała <moskala.michal@gmail.com>
2017-11-27 15:43:17 +0100

    Extracting public method from class

--- extractProtocol.py
+++ extractProtocol.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
 import sys
 
-if len(sys.argv) < 2:
-    print('invalid argument, you need to provide location of Swift file')
+if len(sys.argv) < 3:
+    print('invalid argument, you need to provide location of Swift file and protocol name')
+    exit()
+
+filepath = sys.argv[1]
+protocolName = sys.argv[2]
+
+print 'protocol ' + protocolName + ' {'
+with open(filepath) as f:
+    content = f.readlines()
+    for line in content:
+        if 'func' in line and 'private' not in line:
+            print line.rsplit('{', 1)[0]
+print '}'
